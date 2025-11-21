1d2d0bc8e6eb12d1266c510f92cf8ed866e0690e
siriuslysasi <asoorisasi@gmail.com>
2025-11-21 12:48:35 +0530

    Week1

---
+++ Week1/DayOne/DayOne.js
@@ -0,0 +1,11 @@
+var firstName = 'Sasi';
+var companyName = 'ABC';
+var mobileNumber = 1234567890;
+var isAutomation = null;
+var hasPlaywright;
+
+console.log(`firstName is of dataType ${typeof firstName} and value is ${firstName}`);
+console.log(`companyName is of dataType ${typeof companyName} and value is ${companyName}`);
+console.log(`mobileNumber is of dataType ${typeof mobileNumber} and value is ${mobileNumber}`);
+console.log(`isAutomation is of dataType ${typeof isAutomation} and value is ${isAutomation}`);
+console.log(`hasPlaywright is of dataType ${typeof hasPlaywright} and value is ${hasPlaywright}`);
